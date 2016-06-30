12fc13bde44892a0757e485d60bcbee6cdef831a
Ren <Ren@Rens-MBP.wv.cc.cmu.edu>
2016-06-30 11:41:05 -0400

    added the join page

--- app/views.py
+++ app/views.py
@@ -12,3 +12,7 @@ def about():
 @website.route('/tickets.html')
 def tickets():
   return render_template('tickets.html', title="Buy Tickets")
+
+@website.route('/join.html')
+def join():
+  return render_template('join.html', title="Join Us!")
