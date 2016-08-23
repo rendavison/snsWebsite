f022014792d83fa9592732519a9de58fafa7896b
Ren <Ren@Rens-MBP.wv.cc.cmu.edu>
2016-08-23 15:52:38 -0400

    update run.py with gross hack to make utf8 the default

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 from app import website
+import sys
+
+# Cheaty, cheaty hack to assume utf8 instead of ascii
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# Run the actual site
 website.run(debug=True)
