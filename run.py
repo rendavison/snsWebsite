20661d1960f2c91abf86e9e6de3dddaa06682fba
Remy <Remy@Remys-MacBook-Pro.local>
2017-01-05 10:45:46 -0500

    backup version jan 5

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 Runs the website
 """
 from app import website
+import os
 import sys
 
 # Cheaty, cheaty hack to assume utf8 instead of ascii
@@ -9,4 +10,8 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # Run the actual site
-website.run(debug=True)
+runningOnHeroku = 'PORT' in os.environ
+if runningOnHeroku:
+	website.run(debug=False, host="0.0.0.0", port=os.environ['PORT'])
+else:
+	website.run(debug=True, port=5000)
