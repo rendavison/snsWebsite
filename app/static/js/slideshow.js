ea130d72189f0b8a8f02c51197e8eb909eb5b8e8
Remy <Remy@Remys-Mac.local>
2017-01-06 20:05:40 -0500

    updated slideshow jquery

--- app/static/js/slideshow.js
+++ app/static/js/slideshow.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-  $('.slideshow').slick({
-    infinite: true,
-    autoplay: true,
-    arrows: false,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
+  if ($(window).width() > 667) {
+    $('.slideshow').slick({
+      infinite: true,
+      autoplay: true,
+      arrows: false,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+    }
   });
 });
