c4f1720dab981a65528e104ea1b4ed516ed3f49c
Remy <Remy@Remys-Mac.local>
2017-01-12 18:51:07 -0500

    went back to the beginning because honestly i'm dying

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 Runs the website
 """
 from app import website
-import os
 import sys
 
 # Cheaty, cheaty hack to assume utf8 instead of ascii
@@ -10,8 +9,4 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # Run the actual site
-runningOnHeroku = 'PORT' in os.environ
-if runningOnHeroku:
-	website.run(debug=False, host="0.0.0.0", port=os.environ['PORT'])
-else:
-	website.run(debug=True, port=5000)
+website.run(debug=True)
